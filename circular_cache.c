8ee9ac229e301dcd222ef1936922d98a1f307766
Ali Alrahahleh <aalrahahleh@walmartlabs.com>
2016-12-07 21:39:30 -0800

    add circuler cache

---
+++ circular_cache.c
@@ -0,0 +1,37 @@
+#include  "circular_cache.h"
+#include <stdio.h>
+
+struct circular_cache *circular_cache_find(struct circular_cache *list, char *key, int length) {
+  int i = 0;
+  for(i = 0; i < length; i++) {
+    if(list[i].key) {
+      if(strcmp(key, list[i].key) == 0) {
+        return &list[i];
+      }
+    }
+  }
+  return NULL;
+}
+
+int circular_cache_add(struct circular_cache *list, char *key, void *value, int length)  {
+  struct circular_cache tmp = {strdup(key), value};
+  int i = 0;
+  for(i = 0; i < length; i++) {
+    if(!list[i].key) {
+      printf("added %s\n", key);
+      list[i] = tmp;
+      return 0;
+    }
+  }
+  return -1;
+}
+
+void circular_cache_cleanup(struct circular_cache *list, char *key, void *value, int length, circular_cache_cleanup_cb func) {
+  srand(time(NULL));
+  for(int i = 0; i < length / 2; i++) {
+    int id = rand() % length;
+    if(list[id].key) {
+      func(&list[id]);
+    }
+  }
+}
---
+++ circular_cache.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#pragma once
+
+struct circular_cache {
+  char *key;
+  void *value;
+};
+
+
+
+typedef void circular_cache_cleanup_cb(struct circular_cache *item);
+struct circular_cache *circular_cache_find(struct circular_cache *list, char *key, int length);
+int circular_cache_add(struct circular_cache *list, char *key, void *value, int length);
+void circular_cache_cleanup(struct circular_cache *list, char *key, void *value, int length, circular_cache_cleanup_cb func) ;
