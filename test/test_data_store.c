3e7be5b01462c6f27ee4c074ba23f6d67b9cfbdf
Ali Alrahahleh <aalraha@m-C02J22ZBF1G4.local>
2016-12-31 18:22:58 -0800

    adding store and retrive test

--- test/test_data_store.c
+++ test/test_data_store.c
@@ -6,6 +6,14 @@
 #include <string.h>
 #include <time.h>
 
+struct range_query_result {
+  float *points;
+  int num_shards;
+  time_t shard_size;
+  time_t start_date;
+  int s_type; 
+};
+
 struct M_ITEM {
   int t_time;
   char *name;
@@ -21,8 +29,10 @@ struct M_ITEM metric_list[] = {
 
 
 struct data_store;
+extern struct range_query_result ds_current(struct data_store *dp, char *metric_name, time_t t_time,  int *error);
 extern int store_dp(struct data_store *dp, char *metric_name, time_t point_time, float value);
 extern struct data_store *create_data_store(char *);
+extern time_t init_ds_iter(time_t p_time);
 
 //mock nn_connect
 int nn_connect(int x, char *path) {
@@ -71,9 +81,23 @@ void test_storing_data_point(void **state) {
   will_return(nn_socket, 1);
   struct data_store *dt =  create_data_store("/tmp/cache");
   int test_num = 0;
-  for (int n = 0; n < sizeof(metric_list[0]) / sizeof(metric_list[0]); n++) {
+  for (int n = 0; n < sizeof(metric_list) / sizeof(metric_list[0]); n++) {
     int result = store_dp(dt, metric_list[n].name, metric_list[n].t_time, metric_list[n].value);
     assert_true(result > -1);
+  }
+}
+
+void test_retrieve_data_point(void **state) {
+  will_return(nn_connect, 1);
+  will_return(nn_socket, 1);
+  struct range_query_result r;
+  int error;
+  struct data_store *dt =  create_data_store("/tmp/cache");
+  for (int n = 0; n < sizeof(metric_list) / sizeof(metric_list[0]); n++) {
+    time_t p_time = metric_list[n].t_time;
+    time_t s_start = init_ds_iter(p_time);
+    r = ds_current(dt, metric_list[n].name, s_start, &error);
+    assert_true(r.points[p_time - r.start_date]  ==  metric_list[n].value);
   }
 }
 
@@ -83,6 +107,7 @@ int main(int argc, char *argv[]) {
     cmocka_unit_test(test_creating_data_store_cant_create_socket),
     cmocka_unit_test(test_creating_data_store_cant_connect),
     cmocka_unit_test(test_storing_data_point),
+    cmocka_unit_test(test_retrieve_data_point),
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
